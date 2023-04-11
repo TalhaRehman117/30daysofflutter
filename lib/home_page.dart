8c243a0764726da2515f682d03b018989051b76c
TalhaRehman117 <talhaabbasi730@gmail .com>
2023-04-12 01:19:45 +0500

    day 2 complete

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int days = 30;
+  final String name = "Talha";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TR-Developers"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("(Welcome from Talha Rehman)"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
